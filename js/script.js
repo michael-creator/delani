4bd72808e70da4add200e81a26d2265f8cd42a0d
michael <mikelkarije@gmail.com>
2019-07-24 16:42:24 +0300

    this commit will add some changes and also adeleted gh-page

--- js/script.js
+++ js/script.js
@@ -1,57 +1,65 @@
 $(document).ready(function () {
-            // $("#image1").hover 
-            $("#design").click(function () {
-                $("#designicon").toggle();
-                $("#designdescription").toggle();
-            });
+    // $("#image1").hover 
+    $("#design").click(function () {
+        $("#designicon").toggle();
+        $("#designdescription").toggle();
+    });
 
-            $("#dev").click(function () {
-                $("#devicon").toggle();
-                $("#devdescription").toggle();
-            })
-            $("#product").click(function () {
-                $("#producticon").toggle();
-                $("#productdescription").toggle();
-            })
-            $("#work8").hover(function () {
-                $(".caption8").fadeIn(250);
-            }, function () {
-                $(".caption8").fadeOut(250);
-            });
-            $("#work7").hover(function () {
-                $(".caption7").fadeIn(250);
-            }, function () {
-                $(".caption7").fadeOut(250);
-            });
-            $("#work6").hover(function () {
-                $(".caption6").fadeIn(250);
-            }, function () {
-                $(".caption6").fadeOut(250);
-            });
-            $("#work5").hover(function () {
-                $(".caption5").fadeIn(250);
-            }, function () {
-                $(".caption5").fadeOut(250);
-            });
-            $("#work4").hover(function () {
-                $(".caption4").fadeIn(250);
-            }, function () {
-                $(".caption4").fadeOut(250);
-            });
-            $("#work3").hover(function () {
-                $(".caption3").fadeIn(250);
-            }, function () {
-                $(".caption3").fadeOut(250);
-            });
-            $("#work2").hover(function () {
-                $(".caption2").fadeIn(250);
-            }, function () {
-                $(".caption2").fadeOut(250);
-            });
-            $("#work1").hover(function () {
-                $(".caption1").fadeIn(250);
-            }, function () {
-                $(".caption1").fadeOut(250);
-            });
+    $("#dev").click(function () {
+        $("#devicon").toggle();
+        $("#devdescription").toggle();
+    })
+    $("#product").click(function () {
+        $("#producticon").toggle();
+        $("#productdescription").toggle();
+    })
+    $("#work8").hover(function () {
+        $(".caption8").fadeIn(250);
+    }, function () {
+        $(".caption8").fadeOut(250);
+    });
+    $("#work7").hover(function () {
+        $(".caption7").fadeIn(250);
+    }, function () {
+        $(".caption7").fadeOut(250);
+    });
+    $("#work6").hover(function () {
+        $(".caption6").fadeIn(250);
+    }, function () {
+        $(".caption6").fadeOut(250);
+    });
+    $("#work5").hover(function () {
+        $(".caption5").fadeIn(250);
+    }, function () {
+        $(".caption5").fadeOut(250);
+    });
+    $("#work4").hover(function () {
+        $(".caption4").fadeIn(250);
+    }, function () {
+        $(".caption4").fadeOut(250);
+    });
+    $("#work3").hover(function () {
+        $(".caption3").fadeIn(250);
+    }, function () {
+        $(".caption3").fadeOut(250);
+    });
+    $("#work2").hover(function () {
+        $(".caption2").fadeIn(250);
+    }, function () {
+        $(".caption2").fadeOut(250);
+    });
+    $("#work1").hover(function () {
+        $(".caption1").fadeIn(250);
+    }, function () {
+        $(".caption1").fadeOut(250);
+    });
 
-            });
+    $(".form-style").submit(function() {
+        var inputFirstname = $(".firstname").val();
+        var inputSecondname = $(".secondname").val();
+        var inputComments = $(".comments").val();
+
+        alert("HI " + inputFirstname + " THANKS FOR CONTACTING US WE HAVE RECIEVED YOUR MESSAGE");
+    })
+
+});
