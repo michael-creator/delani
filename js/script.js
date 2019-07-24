ae741cd6303f49dc417b1b565fcfe06a9a7204ce
michael <mikelkarije@gmail.com>
2019-07-24 14:32:13 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
-$(document).ready(function(){
-    // $("#image1").hover 
-    $("#design").click(function(){
-        $("#designicon").toggle();$("#dev").click(function(){
-            $("#devicon").toggle();
-            $("devdescription").toggle();
-        })
-        $("#designdescription").toggle();
-    });
-    $("#dev").click(function(){
-        $("#devicon").toggle();
-        $("#devdescription").toggle();
-    })
-    $("#product").click(function(){
-        $("#producticon").toggle();
-        $("#productdescription").toggle();
-    })
-    $("#work5").hover(function(){
-        $(".caption").fadeIn(250);
-    }, function(){
-        $(".caption").fadeOut(250);
-    }
-    )
-});
+$(document).ready(function () {
+            // $("#image1").hover 
+            $("#design").click(function () {
+                $("#designicon").toggle();
+                $("#designdescription").toggle();
+            });
 
-  
+            $("#dev").click(function () {
+                $("#devicon").toggle();
+                $("#devdescription").toggle();
+            })
+            $("#product").click(function () {
+                $("#producticon").toggle();
+                $("#productdescription").toggle();
+            })
+            $("#work5").hover(function () {
+                $(".caption5").fadeIn(250);
+            }, function () {
+                $(".caption5").fadeOut(250);
+            });
+            $("#work4").hover(function () {
+                $(".caption4").fadeIn(250);
+            }, function () {
+                $(".caption4").fadeOut(250);
+            });
+            $("#work3").hover(function () {
+                $(".caption3").fadeIn(250);
+            }, function () {
+                $(".caption3").fadeOut(250);
+            });
+            $("#work2").hover(function () {
+                $(".caption2").fadeIn(250);
+            }, function () {
+                $(".caption2").fadeOut(250);
+            });
+            $("#work1").hover(function () {
+                $(".caption1").fadeIn(250);
+            }, function () {
+                $(".caption1").fadeOut(250);
+            });
+
+            });
